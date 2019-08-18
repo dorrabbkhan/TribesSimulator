94e2da45b1e12e10d2de828c70e5b092d4ac6b73
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-19 01:50:46 +0500

    Better formatting and commenting, pyling cleanup (6.36 to 10.00), changed one variable name

--- TribesSimulator 1.0/gengraph.py
+++ TribesSimulator 1.0/gengraph.py
@@ -1,16 +1,26 @@
-from numpy import genfromtxt
-import matplotlib.pyplot as plt
 """
 Module file to generate graph from the generated
 data for the trait sums of tribes and save to an image
 """
+
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+# import dependencies
+
+
 def genplot():
-    # Function to generate graph and save to png 
-    per_data = genfromtxt('data.csv', delimiter=',')
+    """
+    Function to generate graph and save to png
+    """
+    data = genfromtxt('data.csv', delimiter=',')
+    # generate data from data.csv
+
     plt.xlabel('Year')
     plt.ylabel('Trait value per trait per capita')
     plt.title('Generational trait progression')
-    plt.plot(per_data)
+    # set labels and title
+
+    plt.plot(data)
     plt.savefig('data.png')
     plt.show()
-    
+    # plot data, save to data.png and show plot
