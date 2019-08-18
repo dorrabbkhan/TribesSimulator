d62d3eae944edd84d2367e09d956cdf2690e6205
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-19 00:23:10 +0500

    Better formatting of species.py, removed needless name generation functionality to make code more concise

--- TribesSimulator 1.0/species.py
+++ TribesSimulator 1.0/species.py
@@ -3,7 +3,6 @@ import random
 File for classes to use in the tribe simulator
 """
 
-# Initialize traits and vowels
 TRAITS = (
     'strength',
     'bravery',
@@ -15,103 +14,108 @@ TRAITS = (
     'desire',
     'synergy'
 )
-vowels = ('a', 'e', 'i', 'o', 'u')
 
+TRIBES = (
+    'Tribe 1',
+    'Tribe 2',
+    'Tribe 3',
+    'Tribe 4',
+    'Tribe 5',
+)
+# initialize arbitrary traits, tribe names
 
-def consgen():
-    # Generate consonants
-    cons = []
-    for i in range(26):
-        if chr(97 + i) not in vowels:
-            cons.append(chr(97 + i))
-    return tuple(cons)
-
-def gen_name():
-    # Generate name as a sequence of consonants and vowels
-    len = random.randint(5, 7)
-    name = ''
-    name += random.choice(cons)
-    for i in range(len - 1):
-        if i % 2 == 0:
-            name += random.choice(vowels)
-        else:
-            name += random.choice(cons)
-    return name
 
 class Tribe:
-    # Tribe class
+    """
+    Tribe class
+    """
+
     def __init__(self, id):
-        # Attributes are name, tribe ID and set of warriors ID's
+        """
+        Attributes are name, tribe ID and set of warriors ID's
+        """
         self.name = TRIBES[id]
         self.id = id
         self.warriors = set()
 
     def add_warrior(self, warriorid):
-        # Add warrior to tribe
+        """
+        Add warrior to tribe
+        """
         self.warriors.add(warriorid)
 
     def kill_warrior(self, warriorid):
-        # Remove warrior from tribe
+        """
+        Remove warrior from tribe
+        """
         self.warriors.remove(warriorid)
 
     def __len__(self):
-        # Return number of warriors in tribe
+        """
+        Return number of warriors in tribe
+        """
         return len(self.warriors)
 
     def random_warrior(self):
-        # Return a random warrior from tribe
+        """
+        Return a random warrior from tribe
+        """
         warrior = self.warriors.pop()
         self.warriors.add(warrior)
         return warrior
 
     def __str__(self):
-        # Return a text representation of the tribe
+        """
+        Return a text representation of the tribe
+        """
         ret = f"Tribe's name is {self.name}\n"
         ret += f"Tribe's warriors are {sorted(self.warriors)}\n"
         return ret
 
 
 class Warrior:
-    # Warrior class
+    """
+    Warrior class
+    """
     def __init__(self, tribe, friends, id):
-        # Attributes are tribe, traits, set of friends, age and ID
+        """
+        Attributes are tribe, traits, set of friends, age and ID
+        """
         self.tribe = tribe
         self.traits = {trait: random.randint(-12, 15) for trait in TRAITS}
         self.friends = friends
         self.age = 0
         self.id = id
 
-    def increase_trait(self, trait, val):
-        # Increase a certain trait
+    def increase_trait(self, trait, inc):
+        """
+        Increase a trait's value by inc (max trait value is 100)
+        """
         if (self.traits[trait] +
-                val) < 100 and (self.traits[trait] + val) > -100:
-            self.traits[trait] += val
-        elif val > 0:
+                inc) < 100 and (self.traits[trait] + inc) > -100:
+            self.traits[trait] += inc
+        elif inc > 0:
             self.traits[trait] = 100
         else:
             self.traits[trait] = -100
 
     def add_friend(self, friend):
-        # Add a friend to friend list
+        """
+        Add a friend to friend list
+        """
         if friend not in self.friends:
             self.friends.add(friend)
 
     def del_friend(self, friend):
-        # Remove a friend from friend list
+        """
+        Remove a friend from friend list
+        """
         if friend in self.friends:
             self.friends.remove(friend)
 
     def inc_age(self):
-        # Increment age
+        """
+        Increment age
+        """
         self.age += 1
         return self.age >= 60
-
-    def __str__(self):
-        # Return text representation of the warrior
-        ret = f"ID = {self.id}\nTribe = {TRIBES[self.tribe]}\n"
-        ret += f"Friends = {self.friends}\n"
-        ret += f"Traits = {self.traits}\n"
-        return ret
-
-cons = consgen()
-TRIBES = [gen_name() for n in range(5)]
